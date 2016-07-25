1e4301cbc4cf1efd0e222e2e0cdfef687adfa7b4
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-07-25 11:14:23 -0500

    Create index.jsx

---
+++ index.jsx
@@ -0,0 +1,24 @@
+const {Link, Route, Router, hashHistory} = ReactRouter;
+
+const App = () =>
+  <div>
+    <h1>App</h1>
+    <Link to="sub-page">Sub Page</Link>
+  </div>
+
+const SubPage = () =>
+  <div>
+    <h1>Sub Page</h1>
+  </div>;
+  
+const Routes = () =>
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <Route path="sub-page" component={SubPage} />
+    </Route>
+  </Router>
+
+ReactDOM.render(
+  <Routes />,
+  document.getElementById('example')
+);
